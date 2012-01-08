a43e0633d1f36d0c9700340abf6d03d67ee9eee7
Damien Cornu <damienc.cornu@gmail.com>
2012-01-08 19:17:35 +0100

    Add an author and a title

--- testSave.php
+++ testSave.php
@@ -1,12 +1,17 @@
 <?php
-if (isset($GLOBALS["HTTP_RAW_POST_DATA"]))
+print_r($_POST);
+if (isset($_POST['canvasData']) && isset($_POST['pseudo']) && isset($_POST['nomCreation']))
 {
+   print_r($_POST);
 	// Get the data
-	$imageData=$GLOBALS['HTTP_RAW_POST_DATA'];
+	$imageData=$_POST['canvasData'];
+	$pseudo = $_POST['pseudo'];
+	$nomCreation = $_POST['nomCreation'];
+	
 
 	// Remove the headers (data:,) part.  
 	// A real application should use them according to needs such as to check image type
-	$filteredData=substr($imageData, strpos($imageData, ",")+1);
+	/*$filteredData=substr($imageData, strpos($imageData, ",")+1);
 
 	// Need to decode before saving since the data we received is already base64 encoded
 	$unencodedData=base64_decode($filteredData);
@@ -20,10 +25,13 @@ if (isset($GLOBALS["HTTP_RAW_POST_DATA"]))
 	fwrite( $fp, $unencodedData);
 	fclose( $fp );
 	
-	$datas = array(
-	  "url" => $a
-	);
+	
+	$retour = '<div class="floatLeft" id="splashFin">';
+	$retour .= '<img src="img/creations/'.$a.'.png" />';
+	$retour .= '';
+	$retour .= '</div><!-- fin #splahsFin -->';
 	
 	   echo json_encode($datas);
+	   */
 }
 ?>
